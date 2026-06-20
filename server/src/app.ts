c49a572da84b0be6a25a1bcce40d0bcf5eb57cba
Kashish Bhutia <kashishbhutia2709@gmail.com>
2026-06-20 14:31:37 +0530

    fix cors issue

--- server/src/app.ts
+++ server/src/app.ts
@@ -9,10 +9,9 @@ import chatRoutes from './routes/chat.routes';
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  origin: "*",
   credentials: true,
-}));
-app.use(express.json({ limit: '50mb' }));
+}));app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/documents', documentRoutes);
